a7c36f5c4232c618db2358a6eaf90a2da446dd8b
Dandara Estrela <31369111+Dandaraestrela@users.noreply.github.com>
2020-07-29 16:15:08 -0300

    Busca e cadastro de pontos de coleta
    Página com cadastro completo para criação do ponto de coleta, funcionalidade da pesquisa por nome de um ponto e página com resultado da busca.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -34,13 +34,15 @@ function getCities(event){
     // input foi criado p não ficar aquele dado na url
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text
-
+    citySelect.innerHTML = "<option value> Selecione a Cidade</option>"
+    citySelect.disabled = true
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
     fetch(url)
     .then((res) => {return res.json()})
     .then( cities => { 
+        
         for(city of cities){
-            citySelect.innerHTML += `<option value= "${city.id}"> ${city.nome} </option>`
+            citySelect.innerHTML += `<option value= "${city.nome}"> ${city.nome} </option>`
         }
         citySelect.disabled = false
     })
@@ -51,3 +53,45 @@ document.querySelector("select[name = uf]")
     // ele faça a função, se eu tivesse colocado getCities() com parenteses
     // ele iria já execultar, mas querermos que ele espere
     .addEventListener("change", getCities)
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = [];
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+    // adicionar ou remover uma classe, para quando clicar no item
+    // utilizando o classList permite que use as classes de forma mais simples
+    // ao inves do toggle poderia ser .add ou .remove mas o toggle faz isso
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+    const alreadySelected = selectedItems.findIndex( item =>{
+        // essa função vai iterar pelo array procurando o elemento igual,
+        // p permitir que o findIndex dê o index
+        return item == itemId
+    })
+
+    if(alreadySelected >= 0){
+        // nesse caso ele já esta na seleção e foi clicado novamente, então precisamos retirar
+        const filteredItems = selectedItems.filter( item => {
+            // essa função vai iterar pela outra, e no final devolve um array novo
+            // para filteredItems, só com o que vai continuar
+            return item != itemId // como o que vai retornar true é tudo o que não é o que foi selecionado de novo, filtra    
+        })
+        selectedItems = filteredItems
+    }else{
+        // nesse caso o elemento nao esta no array
+        selectedItems.push(itemId)
+    }
+    // atualiza sempre ao fim p pegar so o que foi selecionado
+    collectedItems.value = selectedItems
+    
+}
+// Itens de coleta, precisamos adicionar evento a todos os itens
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+for (item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem);
+}
+
+
+
